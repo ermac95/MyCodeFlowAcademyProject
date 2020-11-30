e2b25ca9895f9c192620d9226dbc596cfba4daf0
CodeRow <73800807+ermac95@users.noreply.github.com>
2020-11-30 22:59:28 +0300

    Update MainActivity.kt

--- app/src/main/java/com/mycodeflow/academyproject/MainActivity.kt
+++ app/src/main/java/com/mycodeflow/academyproject/MainActivity.kt
@@ -8,42 +8,26 @@ class MainActivity : AppCompatActivity(), FragmentMoviesList.DetailsListener, Fr
 
     private val listFragment = FragmentMoviesList()
     private var detailsFragment = FragmentMoviesDetails()
-    private var firstLaunch = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null){
             supportFragmentManager.beginTransaction()
-                    .apply {
-                        add(R.id.main_frame_container, listFragment)
-                        addToBackStack(listFragment.toString())
-                        commit()
-                    }
+                    .add(R.id.main_frame_container, listFragment)
+                    .addToBackStack(listFragment.toString())
+                    .commit()
         }
     }
 
     override fun showDetails() {
         supportFragmentManager.beginTransaction()
-                .apply {
-                    add(R.id.main_frame_container, detailsFragment, "detailsFragment")
-                    addToBackStack(detailsFragment.toString())
-                    commit()
-                }
+                .add(R.id.main_frame_container, detailsFragment)
+                .addToBackStack(detailsFragment.toString())
+                .commit()
     }
 
     override fun backToMainMenu() {
-        supportFragmentManager.beginTransaction()
-                .apply {
-                    replace(R.id.main_frame_container, listFragment)
-                    addToBackStack(null)
-                    commit()
-                }
+        supportFragmentManager.popBackStack()
     }
-
-    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
-        super.onSaveInstanceState(outState, outPersistentState)
-        outState.putBoolean("firstLaunch", firstLaunch)
-    }
-
 }
