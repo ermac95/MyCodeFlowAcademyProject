28470fdc4ee7a30806eefd0fab601cdd844a67b2
CodeRow <73800807+ermac95@users.noreply.github.com>
2020-11-30 23:00:38 +0300

    Update FragmentMoviesList.kt

--- app/src/main/java/com/mycodeflow/academyproject/FragmentMoviesList.kt
+++ app/src/main/java/com/mycodeflow/academyproject/FragmentMoviesList.kt
@@ -10,27 +10,26 @@ import androidx.constraintlayout.widget.ConstraintLayout
 
 class FragmentMoviesList : Fragment() {
 
-    private var movieCard : ConstraintLayout? = null
     private var listener : DetailsListener? = null
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_movies_list, container, false)
-        movieCard = view.findViewById<ConstraintLayout>(R.id.movie_item_parent).apply{
-            setOnClickListener{
-                listener?.showDetails()
-            }
-        }
-        return view
-    }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
         if (context is DetailsListener){
             listener = context
         }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_movies_list, container, false)
+        view.findViewById<ConstraintLayout>(R.id.movie_item_parent)?.apply{
+            setOnClickListener{
+                listener?.showDetails()
+            }
+        }
+        return view
     }
 
     override fun onDetach() {
