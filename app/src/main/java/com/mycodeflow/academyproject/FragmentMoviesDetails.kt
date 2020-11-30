9f4c511cea75d94bf5f30568052c40ac18a05459
CodeRow <73800807+ermac95@users.noreply.github.com>
2020-11-30 23:00:06 +0300

    Update FragmentMoviesDetails.kt

--- app/src/main/java/com/mycodeflow/academyproject/FragmentMoviesDetails.kt
+++ app/src/main/java/com/mycodeflow/academyproject/FragmentMoviesDetails.kt
@@ -10,27 +10,25 @@ import android.widget.ImageView
 
 class FragmentMoviesDetails : Fragment() {
 
-    private var listener: BackToMenuListener? = null
-    private var backToMenu : ImageView? = null
-
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_movies_details, container, false)
-        backToMenu = view.findViewById<ImageView>(R.id.movie_details_back_button)
-            .apply {
-                setOnClickListener{
-                    listener?.backToMainMenu()
-                }
-            }
-        return view
-    }
+   private var listener: BackToMenuListener? = null
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
         if (context is BackToMenuListener){
             listener = context
         }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_movies_details, container, false)
+        view.findViewById<ImageView>(R.id.movie_details_back_button)
+            ?.apply {
+                setOnClickListener{
+                    listener?.backToMainMenu()
+                }
+            }
+        return view
     }
 
     override fun onDetach() {
@@ -41,5 +39,4 @@ class FragmentMoviesDetails : Fragment() {
     interface BackToMenuListener{
         fun backToMainMenu()
     }
-
 }
