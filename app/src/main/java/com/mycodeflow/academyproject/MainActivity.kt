94e9aa067bb0b8de262f945780018ad42d1a17f5
CodeRow <73800807+ermac95@users.noreply.github.com>
2020-12-15 21:21:39 +0300

    Update MainActivity.kt

--- app/src/main/java/com/mycodeflow/academyproject/MainActivity.kt
+++ app/src/main/java/com/mycodeflow/academyproject/MainActivity.kt
@@ -5,30 +5,25 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity(), FragmentMoviesDetails.BackToMenuListener, FragmentMoviesList.MovieDetailsListener {
 
-    private val listFragment = FragmentMoviesList()
-    private var detailsFragment = FragmentMoviesDetails()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null){
             supportFragmentManager.beginTransaction()
-                    .add(R.id.main_frame_container, listFragment)
-                    .addToBackStack(listFragment.toString())
+                    .add(R.id.main_frame_container, FragmentMoviesList.newInstance())
+                    .addToBackStack(FragmentMoviesList.toString())
                     .commit()
         }
     }
 
     override fun showDetails(movieId: Int) {
-        val detailsFragment = FragmentMoviesDetails.newInstance(movieId)
         supportFragmentManager.beginTransaction()
-                .add(R.id.main_frame_container, detailsFragment)
-                .addToBackStack(detailsFragment.toString())
+                .add(R.id.main_frame_container, FragmentMoviesDetails.newInstance(movieId))
+                .addToBackStack(FragmentMoviesDetails.toString())
                 .commit()
     }
 
     override fun backToMainMenu() {
         supportFragmentManager.popBackStack()
     }
-
 }
